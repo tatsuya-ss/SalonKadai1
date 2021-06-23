6503180283d2f7155c4cd0564657d8726588efc0
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-06-23 11:11:59 +0900

    CalculateファイルとStoryboardを作成

---
+++ SalonKadai1/Calculate/CalculateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalculateViewController.swift
+//  SalonKadai1
+//
+//  Created by 坂本龍哉 on 2021/06/23.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
